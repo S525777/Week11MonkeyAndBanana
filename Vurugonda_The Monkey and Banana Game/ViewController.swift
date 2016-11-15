3886e8ece5378c77ced4fd7c9226aceaca12a885
Student <student@ch140013m.nwmissouri.edu>
2016-11-15 17:03:40 -0600

    Initial Commit

---
+++ Vurugonda_The Monkey and Banana Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vurugonda_The Monkey and Banana Game
+//
+//  Created by Student on 11/15/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
